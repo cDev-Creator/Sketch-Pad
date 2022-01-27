6eb40a9b8a0ca330e4a043844ff342809773b00e
cDev-Creator <mccoyng170@gmail.com>
2022-01-27 17:40:49 -0500

    grid scaling correctly

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
 const container = document.getElementById('container')
-const gridItem = document.querySelector('grid-item')
+
 const randColorsButton = document.getElementById('random-colors-button')
 
 function makeGridItem(numItems) {
 
     for (let i = 0; i < (numItems); i++) {
       let cell = document.createElement("div")
-      cell.innerText = (i + 1)
       cell.className =  "grid-item"
       container.appendChild(cell)
-     
-    }
-    
-    container.addEventListener('mouseover', e =>{
-        if(e.target.matches = 'div.grid-item') {
-            e.target.style.backgroundColor = 'grey' 
-        } 
-    })
+    } 
 }
 
-function gridCreator(rows, columns) {
+function createGrid(rows, columns) {
     container.style.setProperty('--grid-rows', rows)
     container.style.setProperty('--grid-columns', columns)
-    for(let i = 0; i < rows; i++) {
-        makeGridItem(columns)
+    for(let i = 0; i < columns; i++) {
+        makeGridItem(rows)
     }
 }
 
 function promptGridSize() {
+    
 
-    let usersNum = prompt("Enter a number 1-64", 16)
-    if(usersNum <= 64) {
-        gridCreator(usersNum, usersNum)
+    let usersNum = prompt("To resize the pixels enter a number 5-70", 16)
+    if(usersNum <= 70 && usersNum >= 5) {
+        createGrid(usersNum, usersNum)
     }
     else {
-        usersNum = prompt("Invalid size! Your value must be between 1-64!");
+        do{
+            usersNum = prompt("Invalid size! Your value must be between 5-70!")
+        }
+        while(usersNum > 70 || usersNum < 5)
     }
+    createGrid(usersNum, usersNum)
+
 }
+
+container.addEventListener('mouseover', e =>{
+    e.target = 'div.grid-item'
+    /* if(e.target.matches = 'div.grid-item') { */
+        e.target.style.backgroundColor = 'grey' 
+/*     }  */
+})
 
 randColorsButton.addEventListener('click', e =>{
     container.addEventListener('mouseover', e =>{ 
